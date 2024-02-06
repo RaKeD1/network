d0a55a6248ee14de5a89ac040f316d8cc984ec74
RaKeD1 <121726628+RaKeD1@users.noreply.github.com>
2024-02-07 02:36:16 +0300

    Добавил eslint, fix всех файлов

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [
+                '.eslintrc.{js,cjs}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+
+    plugins: [
+        'react',
+        '@typescript-eslint',
+    ],
+    rules: {
+        'react/jsx-indent': [2, 4],
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'react/jsx-indent-props': [2, 4],
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': 'warn',
+        indent: [2, 4],
+        'react/require-default-props': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'react/function-component-definition': 'off',
+        'no-shadow': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'warn',
+        'no-underscore-dangle': 'warn',
+    },
+    globals: {
+        __IS_DEV__: true,
+    },
+};
