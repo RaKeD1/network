87bbf1083836b5ad84f0e2999996e36d88290f9b
RaKeD1 <121726628+RaKeD1@users.noreply.github.com>
2024-03-25 03:46:26 +0300

    Добавил тесты для login формы

---
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { userActions } from 'entities/User';
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
+import { loginByUsername } from './loginByUsername';
+
+jest.mock('axios');
+
+const mockedAxios = jest.mocked(axios, true);
+describe('loginByUsername', () => {
+    test('success login', async () => {
+        const userValue = { username: 'admin', id: '1' };
+        mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
+
+        const thunk = new TestAsyncThunk(loginByUsername);
+        const result = await thunk.callThunk({ username: 'admin', password: '1234' });
+
+        expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
+        expect(thunk.dispatch).toHaveBeenCalledTimes(3);
+        expect(mockedAxios.post).toBeCalled();
+        expect(result.meta.requestStatus).toBe('fulfilled');
+        expect(result.payload).toEqual(userValue);
+    });
+
+    test('error login', async () => {
+        mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
+
+        const thunk = new TestAsyncThunk(loginByUsername);
+        const result = await thunk.callThunk({ username: 'admin', password: '1234' });
+
+        expect(mockedAxios.post).toBeCalled();
+        expect(thunk.dispatch).toHaveBeenCalledTimes(2);
+        expect(result.meta.requestStatus).toBe('rejected');
+        expect(result.payload).toBe('error');
+    });
+});
---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,25 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from 'features/AuthByUsername';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice', () => {
+    test('test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: '1234' };
+
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setUsername('12341234'),
+        ))
+            .toEqual({ username: '12341234' });
+    });
+
+    test('test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '1234' };
+
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setPassword('12341234'),
+        ))
+            .toEqual({ password: '12341234' });
+    });
+});
