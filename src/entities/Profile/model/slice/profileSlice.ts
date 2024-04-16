da830070568d5704bb7838b6f27b3532414e03b2
RaKeD1 <121726628+RaKeD1@users.noreply.github.com>
2024-04-16 06:47:57 +0300

    Добавил страницу с профилем

--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { ProfileSchema } from '../types/profile';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { fetchProfileData } from '../services/fetchProfileData/fetchProfileData';
+import { Profile, ProfileSchema } from '../types/profile';
 
 const initialState: ProfileSchema = {
     data: undefined,
@@ -12,6 +13,21 @@ const profileSlice = createSlice({
     name: 'profile',
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchProfileData.pending, (state) => {
+            state.isLoading = true;
+            state.error = undefined;
+        });
+        builder.addCase(fetchProfileData.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        });
+        builder.addCase(fetchProfileData.fulfilled, (state, action: PayloadAction<Profile>) => {
+            state.isLoading = false;
+            state.error = undefined;
+            state.data = action.payload;
+        });
+    },
 });
 
 export const {
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,19 +1,21 @@
-import React, { memo } from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { memo, useEffect } from 'react';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { profileReducer } from 'entities/Profile';
+import { fetchProfileData, ProfileCard, profileReducer } from 'entities/Profile';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 
 const reducers:ReducersList = {
     profile: profileReducer,
 };
 
 const ProfilePage = memo(() => {
-    const { t } = useTranslation('profile');
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
+
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-            <div>
-                {t('Профиль')}
-            </div>
+            <ProfileCard />
         </DynamicModuleLoader>
 
     );
